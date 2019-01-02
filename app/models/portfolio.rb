ac0c49eacc36b08d47373b901502a57f10aa63af
navivix <>
2019-01-02 21:59:05 +0100

    Integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,6 +3,8 @@ class Portfolio < ApplicationRecord
 
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  has_many :technologies
+
   def self.angular
     where(subtitle: 'Angular')
   end
