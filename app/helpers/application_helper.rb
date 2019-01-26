d2faf572d5c116a92dfa8ef19e31516377295975
navivix <>
2019-01-26 20:35:11 +0100

    Refactored alerts into helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -60,4 +60,18 @@ module ApplicationHelper
   def active? path
     'active' if current_page? path
   end
+
+  def alerts
+    alert = (flash[:alert] or flash[:error] or flash[:notice])
+
+    if alert
+      alert_generator(alert)
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: 'Notice', sticky: false)
+  end
+
+
 end
