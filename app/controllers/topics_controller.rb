fe8a58965a04f0e4dc62bf56197d387ab6448fdd
navivix <gkaczmarek3@icloud.com>
2019-03-25 15:15:29 +0100

    Implemented topics widget for blog page

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,8 @@
 class TopicsController < ApplicationController
   layout 'blog'
 
+  before_action :set_sidebar_topics
+
   def index
     @topics = Topic.all
   end
@@ -13,5 +15,11 @@ class TopicsController < ApplicationController
     else
       @blogs = @topic.blogs.recent.published.page(params[:page]).per(5)
     end
+  end
+
+  private
+
+  def set_sidebar_topics
+    @sidebar_topics = Topic.with_blogs(logged_in?(:site_admin))
   end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,12 @@ class Topic < ApplicationRecord
   validates_presence_of :title
 
   has_many :blogs
+
+  def self.with_blogs site_admin_logged_in
+    if site_admin_logged_in
+      includes(:blogs).where.not(blogs: { id: nil }).order(:title)
+    else
+      includes(:blogs).where.not(blogs: { id: nil, status: 0 }).order(:title)
+    end
+  end
 end
